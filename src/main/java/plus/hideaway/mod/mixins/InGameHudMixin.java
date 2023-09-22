6348d505386370319bab6f90ae578691f0c7be80
Blobanium <45613796+Blobanium@users.noreply.github.com>
2023-09-22 15:24:32 -0700

    1.20.1

--- src/main/java/plus/hideaway/mod/mixins/InGameHudMixin.java
+++ src/main/java/plus/hideaway/mod/mixins/InGameHudMixin.java
@@ -1,12 +1,9 @@
 package plus.hideaway.mod.mixins;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,10 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import plus.hideaway.mod.HideawayPlus;
 import plus.hideaway.mod.util.Chars;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -28,11 +22,10 @@ public class InGameHudMixin {
     private final HashMap<Text, Integer> activeToasts = new HashMap<>();
 
     @Inject(at = @At("HEAD"), method = "render")
-    public void onRender(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+    public void onRender(DrawContext context, float tickDelta, CallbackInfo ci) {
         if (HideawayPlus.config().jukebox()) {
             if (HideawayPlus.jukebox().currentTrack != null) {
-                DrawableHelper.drawTextWithShadow(
-                        matrices,
+                context.drawTextWithShadow(
                         MinecraftClient.getInstance().textRenderer,
                         Text.empty()
                             .append(Chars.disc())
