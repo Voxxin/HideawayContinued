d54a9a3cebdc838051714191793f1fe8b253df5c
Ella <59192970+Voxxin@users.noreply.github.com>
2023-10-02 20:07:47 -0400

    Hovered Slot fix & Wardrobe WIP

--- src/main/java/continued/hideaway/mod/mixins/AbstractClientPlayerMixin.java
+++ src/main/java/continued/hideaway/mod/mixins/AbstractClientPlayerMixin.java
@@ -7,6 +7,8 @@ import continued.hideaway.mod.util.StaticValues;
 import net.minecraft.client.multiplayer.PlayerInfo;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,6 +18,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.UUID;
+import java.util.function.BiConsumer;
 
 @Mixin(AbstractClientPlayer.class)
 public class AbstractClientPlayerMixin {
@@ -35,6 +38,11 @@ public class AbstractClientPlayerMixin {
 
             AbstractClientPlayer fakePlayer = createFakePlayer();
             fakePlayer.setPos(player.getX(), HideawayPlus.player().getY() + 1, player.getZ());
+
+            for (EquipmentSlot slot : EquipmentSlot.values()) {
+                ItemStack stack = player.getItemBySlot(slot);
+                fakePlayer.setItemSlot(slot, stack);
+            }
 
             player.discard();
             player.kill();
