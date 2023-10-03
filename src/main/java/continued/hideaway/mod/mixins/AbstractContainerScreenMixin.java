617d6f0492fae25b5763f6375705407a197a13ba
drago <dragonostic@gmail.com>
2023-10-03 16:17:52 -0700

    Fixes armor slot inventory colors, but creates an issue in chests where the color allows you to see through the chest

--- src/main/java/continued/hideaway/mod/mixins/AbstractContainerScreenMixin.java
+++ src/main/java/continued/hideaway/mod/mixins/AbstractContainerScreenMixin.java
@@ -1,7 +1,6 @@
 package continued.hideaway.mod.mixins;
 
 import blue.endless.jankson.annotation.Nullable;
-import continued.hideaway.mod.HideawayPlus;
 import continued.hideaway.mod.feat.ext.AbstractContainerScreenAccessor;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
@@ -22,35 +21,55 @@ public abstract class AbstractContainerScreenMixin implements AbstractContainerS
     @Final
     @Shadow
     protected AbstractContainerMenu menu;
-    @Shadow protected int leftPos;
-    @Shadow protected int topPos;
 
     @Shadow protected Slot hoveredSlot;
 
-    @Inject(method = "render", at = @At("TAIL"))
-    public void renderSlotRarity(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
-        if (HideawayPlus.connected() && HideawayPlus.config().inventoryRarities()) {
-            for (int k = 0; k < (this.menu).slots.size(); ++k) {
-                Slot slot = (this.menu).slots.get(k);
-                TextColor itemColor = slot.getItem().getHoverName().getStyle().getColor();
-                if (itemColor != null) {
-                    int color = itemColor.getValue();
-                    int r = (color >> 16) & 0xFF;
-                    int g = (color >> 8) & 0xFF;
-                    int b = color & 0xFF;
+//    @Inject(method = "render", at = @At("TAIL"))
+//    public void renderSlotRarity(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
+//        if (HideawayPlus.connected() && HideawayPlus.config().inventoryRarities()) {
+//            for (int k = 0; k < (this.menu).slots.size(); ++k) {
+//                Slot slot = (this.menu).slots.get(k);
+//                TextColor itemColor = slot.getItem().getHoverName().getStyle().getColor();
+//                if (itemColor != null) {
+//                    int color = itemColor.getValue();
+//                    int r = (color >> 16) & 0xFF;
+//                    int g = (color >> 8) & 0xFF;
+//                    int b = color & 0xFF;
+//
+//
+//                    int itemColour = FastColor.ARGB32.color(150, r, g, b);
+//                    int leftX = leftPos + slot.x;
+//                    int leftY = topPos + slot.y;
+//
+//                    guiGraphics.fill(leftX, leftY + 2, leftX + 1, leftY + 14, itemColour);
+//                    guiGraphics.fill(leftX + 1, leftY + 1, leftX + 2, leftY + 15, itemColour);
+//                    guiGraphics.fill(leftX + 2, leftY, leftX + 14, leftY + 16, itemColour);
+//                    guiGraphics.fill(leftX + 14, leftY + 1, leftX + 15, leftY + 15, itemColour);
+//                    guiGraphics.fill(leftX + 15, leftY + 2, leftX + 16, leftY + 14, itemColour);
+//                }
+//            }
+//        }
+//    }
+
+    @Inject(method = "renderSlot", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;renderItem(Lnet/minecraft/world/item/ItemStack;III)V"))
+    public void renderSlot(GuiGraphics guiGraphics, Slot slot, CallbackInfo ci) {
+        TextColor itemColor = slot.getItem().getHoverName().getStyle().getColor();
+        if (itemColor != null) {
+            int color = itemColor.getValue();
+            int r = (color >> 16) & 0xFF;
+            int g = (color >> 8) & 0xFF;
+            int b = color & 0xFF;
 
 
-                    int itemColour = FastColor.ARGB32.color(150, r, g, b);
-                    int leftX = leftPos + slot.x;
-                    int leftY = topPos + slot.y;
+            int itemColour = FastColor.ARGB32.color(150, r, g, b);
+            int leftX = slot.x;
+            int leftY = slot.y;
 
-                    guiGraphics.fill(leftX, leftY + 2, leftX + 1, leftY + 14, itemColour);
-                    guiGraphics.fill(leftX + 1, leftY + 1, leftX + 2, leftY + 15, itemColour);
-                    guiGraphics.fill(leftX + 2, leftY, leftX + 14, leftY + 16, itemColour);
-                    guiGraphics.fill(leftX + 14, leftY + 1, leftX + 15, leftY + 15, itemColour);
-                    guiGraphics.fill(leftX + 15, leftY + 2, leftX + 16, leftY + 14, itemColour);
-                }
-            }
+            guiGraphics.fill(leftX, leftY + 2, leftX + 1, leftY + 14, 2, itemColour);
+            guiGraphics.fill(leftX + 1, leftY + 1, leftX + 2, leftY + 15, 2, itemColour);
+            guiGraphics.fill(leftX + 2, leftY, leftX + 14, leftY + 16, 2, itemColour);
+            guiGraphics.fill(leftX + 14, leftY + 1, leftX + 15, leftY + 15, 2, itemColour);
+            guiGraphics.fill(leftX + 15, leftY + 2, leftX + 16, leftY + 14, 2, itemColour);
         }
     }
 
