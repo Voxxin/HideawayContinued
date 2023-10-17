58b16c0e1e5c4ada059740860ceb22eec92399e7
drago <dragonostic@gmail.com>
2023-10-16 23:11:27 -0700

    Fixes /friend command spam

--- src/main/java/continued/hideaway/mod/feat/ui/FriendsListUI.java
+++ src/main/java/continued/hideaway/mod/feat/ui/FriendsListUI.java
@@ -2,72 +2,49 @@ package continued.hideaway.mod.feat.ui;
 
 import continued.hideaway.mod.HideawayPlus;
 import continued.hideaway.mod.feat.ext.AbstractContainerScreenAccessor;
+import continued.hideaway.mod.mixins.ext.ClientPacketListenerAccessor;
 import continued.hideaway.mod.util.StaticValues;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.ContainerScreen;
-import net.minecraft.commands.arguments.ArgumentSignatures;
 import net.minecraft.core.UUIDUtil;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.LastSeenMessages;
-import net.minecraft.network.protocol.game.ServerboundChatCommandPacket;
 import net.minecraft.world.inventory.ChestMenu;
 import net.minecraft.world.inventory.ClickType;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.BitSet;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 public class FriendsListUI {
-    private static ChestMenu oldMenu = null;
-    private static int ticker = 0;
-    private static boolean calledProper = false;
-
     public static void tick() {
+        if (StaticValues.friendsCheck) return;
         Minecraft client = HideawayPlus.client();
 
-        if (client.screen instanceof ContainerScreen && ((ContainerScreen) client.screen).getMenu().getItems().stream().filter(itemStack -> itemStack.getItem() == Items.PLAYER_HEAD).count() > StaticValues.friendsUsernames.size() - 1)
-            StaticValues.friendsCheck = false;
-
-        if (!StaticValues.friendsUUID.contains(client.player.getStringUUID()))
-            StaticValues.friendsUUID.add(client.player.getStringUUID());
-        if (!StaticValues.friendsUsernames.contains(client.player.getName().getString()))
-            StaticValues.friendsUsernames.add(client.player.getName().getString());
-        if (StaticValues.friendsCheck) return;
         if (client.screen instanceof ContainerScreen abstractContainerScreen) {
             ChestMenu menu = abstractContainerScreen.getMenu();
-            calledProper = true;
-            if (oldMenu != null && oldMenu == menu) return;
-            oldMenu = menu;
 
-            List<ItemStack> allItems = new ArrayList<>(menu.getItems());
-            boolean hasMorePages = menu.getItems().stream().anyMatch(itemStack -> itemStack.getItem() == Items.PAPER && itemStack.getTag().getAsString().contains("→"));
+            List<ItemStack> items = menu.getItems();
+            boolean hasMorePages = false;
 
-            HideawayPlus.lifecycle().addAsync(
-                    "friendsCheck",
-                    CompletableFuture.runAsync(() -> {
-                        System.out.println("Friends list size: " + StaticValues.friendsUsernames.size());
-                        List<ItemStack> newAllItems = new ArrayList<>(allItems);
-                        for (ItemStack itemStack : newAllItems) {
-                            if (itemStack.getItem() == Items.PLAYER_HEAD) {
-                                CompoundTag tag = itemStack.getTag();
-                                CompoundTag skull = tag.getCompound("SkullOwner");
+            System.out.println("Friends list size: " + StaticValues.friendsUsernames.size());
+            for (ItemStack itemStack : items) {
+                Item item = itemStack.getItem();
+                if (item == Items.PLAYER_HEAD) {
+                    CompoundTag tag = itemStack.getTag();
+                    if (tag.toString().contains("Left click to Accept")) continue;
 
-                                if (tag.toString().contains("Left click to Accept")) continue;
-                                int[] uuidIntArray = skull.getIntArray("Id");
-                                String uuid = UUIDUtil.uuidFromIntArray(uuidIntArray).toString();
-                                if (!StaticValues.friendsUUID.contains(uuid)) StaticValues.friendsUUID.add(uuid);
-                                String name = skull.getString("Name");
-                                if (!StaticValues.friendsUsernames.contains(name))
-                                    StaticValues.friendsUsernames.add(name);
-                            }
-                        }
-                    })
-            );
+                    CompoundTag skull = tag.getCompound("SkullOwner");
+                    int[] uuidIntArray = skull.getIntArray("Id");
+                    String uuid = UUIDUtil.uuidFromIntArray(uuidIntArray).toString();
+                    String name = skull.getString("Name");
+                    if (!StaticValues.friendsUUID.contains(uuid)) StaticValues.friendsUUID.add(uuid);
+                    if (!StaticValues.friendsUsernames.contains(name)) StaticValues.friendsUsernames.add(name);
+                } else if (item == Items.PAPER && itemStack.getTag().getAsString().contains("→")) {
+                    hasMorePages = true;
+                }
+            }
 
             if (!hasMorePages) {
                 StaticValues.friendsCheck = true;
@@ -75,24 +52,10 @@ public class FriendsListUI {
             } else {
                 Slot paperSlot = menu.slots.stream().filter(slot -> slot.getItem().getItem() == Items.PAPER && slot.getItem().getTag().getAsString().contains("→")).findFirst().orElse(null);
                 ((AbstractContainerScreenAccessor) abstractContainerScreen).hp$slotChange(paperSlot, 0, 0, ClickType.PICKUP);
+                tick();
             }
-        } else {
-            if (ticker >= 100 && !calledProper) {
-                LastSeenMessages.Update messages = new LastSeenMessages.Update(0, new BitSet());
-                Instant now = Instant.now();
-                client.player.connection.send(
-                        new ServerboundChatCommandPacket(
-                                "friend",
-                                now,
-                                0L,
-                                ArgumentSignatures.EMPTY,
-                                messages)
-                );
-            } else {
-                ticker++;
-                calledProper = false;
-            }
-
+        } else if (client.getConnection() != null) {
+            ((ClientPacketListenerAccessor) client.getConnection()).hp$sendCommand("friend");
         }
     }
 }
