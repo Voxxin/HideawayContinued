5c5acdb53e63f957ff475823acd09208bec6a55d
drago <dragonostic@gmail.com>
2023-12-17 17:41:20 -0800

    New ConfigUI with tabs. Still need to change SoundConfigModel to use a float value + sliders in ConfigUI

--- src/main/java/continued/hideaway/mod/feat/ui/ConfigUI.java
+++ src/main/java/continued/hideaway/mod/feat/ui/ConfigUI.java
@@ -11,11 +11,15 @@ import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.components.tabs.GridLayoutTab;
 import net.minecraft.client.gui.components.tabs.TabManager;
 import net.minecraft.client.gui.components.tabs.TabNavigationBar;
+import net.minecraft.client.gui.layouts.FrameLayout;
 import net.minecraft.client.gui.layouts.GridLayout;
 import net.minecraft.client.gui.navigation.ScreenRectangle;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import org.jetbrains.annotations.Nullable;
 
 public class ConfigUI extends Screen {
@@ -23,6 +27,9 @@ public class ConfigUI extends Screen {
     private final TabManager tabManager = new TabManager(this::addRenderableWidget, guiEventListener -> this.removeWidget((GuiEventListener)guiEventListener));
     @Nullable
     private TabNavigationBar tabNavigationBar;
+    @Nullable
+    private GridLayout bottomButtons;
+    public static final ResourceLocation FOOTER_SEPERATOR = new ResourceLocation("textures/gui/footer_separator.png");
 
     public ConfigUI(Screen parent) {
         super(Component.translatable("config.hp.general.title"));
@@ -33,14 +40,24 @@ public class ConfigUI extends Screen {
     protected void init() {
         this.tabNavigationBar = TabNavigationBar.builder(this.tabManager, this.width).addTabs(new GeneralTab(), new SoundsTab()).build();
         this.addRenderableWidget(this.tabNavigationBar);
+
+        this.bottomButtons = (new GridLayout()).columnSpacing(10);
+        GridLayout.RowHelper rowHelper = this.bottomButtons.createRowHelper(2);
+        rowHelper.addChild(Button.builder(CommonComponents.GUI_DONE, (button) -> {
+            this.minecraft.setScreen(this.parent);
+        }).build());
+        this.bottomButtons.visitWidgets((widget) -> {
+            widget.setTabOrderGroup(1);
+            this.addRenderableWidget(widget);
+        });
         this.tabNavigationBar.selectTab(0, false);
         this.repositionElements();
     }
 
     @Override
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        this.renderDirtBackground(guiGraphics);
         super.render(guiGraphics, mouseX, mouseY, partialTick);
+        guiGraphics.blit(FOOTER_SEPERATOR, 0, Mth.roundToward(this.height - 36 - 2, 2), 0.0F, 0.0F, this.width, 2, 32, 2);
     }
 
     @Override
@@ -55,15 +72,16 @@ public class ConfigUI extends Screen {
 
     @Override
     public void repositionElements() {
-        if (this.tabNavigationBar == null) return;
-        this.tabNavigationBar.setWidth(this.width);
-        this.tabNavigationBar.arrangeElements();
-
-        int i = this.tabNavigationBar.getRectangle().bottom();
-        ScreenRectangle screenRectangle = new ScreenRectangle(0, i, this.width, this.height);
-        this.tabManager.setTabArea(screenRectangle);
+        if (this.tabNavigationBar != null && this.bottomButtons != null) {
+            this.tabNavigationBar.setWidth(this.width);
+            this.tabNavigationBar.arrangeElements();
+            this.bottomButtons.arrangeElements();
+            FrameLayout.centerInRectangle(this.bottomButtons, 0, this.height - 36, this.width, 36);
+            int i = this.tabNavigationBar.getRectangle().bottom();
+            ScreenRectangle screenRectangle = new ScreenRectangle(0, i, this.width, this.bottomButtons.getY() - i);
+            this.tabManager.setTabArea(screenRectangle);
+        }
     }
-
 
     @Environment(value= EnvType.CLIENT)
     static class GeneralTab
